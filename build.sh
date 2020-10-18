c2d913e02374f8c0ad5693370cb2f0e0339f3db7
hkdb <hkdb@3df.io>
2020-10-19 04:23:24 +0800

    Mac dirty compile hack fix

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sed -i 's/\/\/ \"syscall\"/\"syscall\"/g' main.go
 	sed -i 's/\/\/ cmd.SysProcAttr/cmd.SysProcAttr/g' main.go
 elif [[ "$OSTYPE" == "darwin19" || "$OSTYPE" == "darwin17" ]]; then
-	sed -i 's/\"syscall\"/\/\/ \"syscall\"/g' main.go
-	sed -i 's/cmd.SysProcAttr/\/\/ cmdSysProcAttr/g' main.go
-    	astilectron-bundler
-	sed -i 's/\/\/ \"syscall\"/\"syscall\"/g' main.go
+	sed -i -e 's/\"syscall\"/\/\/ \"syscall\"/g' main.go
+        sed -i -e 's/cmd.SysProcAttr/\/\/ cmd.SysProcAttr/g' main.go
+        astilectron-bundler
+        sed -i -e 's/\/\/ \"syscall\"/\"syscall\"/g' main.go
+        sed -i -e 's/\/\/ cmd.SysProcAttr/cmd.SysProcAttr/g' main.go
+        rm main.go-e
 else
     echo "$OSTYPE is not a supported platform"
 fi
